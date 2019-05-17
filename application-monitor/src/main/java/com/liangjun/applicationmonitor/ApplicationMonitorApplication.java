d2eab85848aa57128cd40430896b7bdaebc91606
liangjun <p_jjunliang>
2019-05-17 17:30:00 +0800

    应用监控项目

--- application-monitor/src/main/java/com/liangjun/applicationmonitor/ApplicationMonitorApplication.java
+++ application-monitor/src/main/java/com/liangjun/applicationmonitor/ApplicationMonitorApplication.java
@@ -2,12 +2,20 @@ package com.liangjun.applicationmonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApplicationMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMonitorApplication.class, args);
+    }
+
+    @RequestMapping("getPeople")
+    public String getPeople(){
+        return "people";
     }
 
 }
